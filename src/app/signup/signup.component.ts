1f4e430d43a2d23f45db7580cf1f6ed626187113
rushanthbala <rushanth9999@gmail.com>
2021-12-20 10:06:18 +0530

    setup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
 export class SignupComponent implements OnInit {
 
   constructor() { }
-
+  signupForm !:FormGroup
   ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      email: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+      cPassword: new FormControl(null, Validators.required),
+    })
+  }
+  onSubmit() {
+    console.log(this.signupForm.value);
+    
   }
 
 }
